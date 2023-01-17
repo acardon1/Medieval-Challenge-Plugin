bee1c2cb50f0921cf82b29a6d2c15773826e6296
Antonio Cardona <46504041+acardon1@users.noreply.github.com>
2023-01-17 02:14:08 -0500

    Reformatting POM.xml
    Currently unable to compile bc I messed up the pom

--- MedievalChallengePlugin/src/main/java/io/github/acardon1/commandexecutors/MedievalTeams.java
+++ MedievalChallengePlugin/src/main/java/io/github/acardon1/commandexecutors/MedievalTeams.java
@@ -1,6 +1,10 @@
 package io.github.acardon1.commandexecutors;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.Command;
+
+import org.bukkit.command.CommandSender;
 import org.bukkit.scoreboard.*;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.ScoreboardManager;
@@ -9,16 +13,20 @@ import org.bukkit.scoreboard.Team;
 public class MedievalTeams implements CommandExecutor {
 
     @Override
-    public boolean onCommand(Command command, String label, String[] args){
+    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args){
         ScoreboardManager manager = Bukkit.getScoreboardManager();
-        Scoreboard board = manager.getNewScoreboard();
-        Team team1 = board.registerNewTeam("TeamA");
-        Team team2 = board.registerNewTeam("TeamB");
-        Team team3 = board.registerNewTeam("TeamC");
-        Team team4 = board.registerNewTeam("TeamD");
+        Scoreboard simulation = manager.getNewScoreboard();
+        Team team1 = simulation.registerNewTeam("TeamA");
+        Team team2 = simulation.registerNewTeam("TeamB");
+        Team team3 = simulation.registerNewTeam("TeamC");
+        Team team4 = simulation.registerNewTeam("TeamD");
         /*make lives for all teams (im taking in an arg for lives,
-        *but should prolly renamed to startgame or sm.)
+        * but should prolly renamed to startgame or sm.)
+        * also should learn how to read world name i think
+        * maybe make a fancy display??
         */
         //assign players amongst each team randomly -> 1 3 2 4
+        return true;
     }
+
 }
